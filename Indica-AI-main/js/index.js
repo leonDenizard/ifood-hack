9bac1ed7a6423ec024f645e7f03ec1d71dc6c97b
Leon <de.nizard@hotmail.com>
2024-07-04 17:31:26 -0300

    V2

--- Indica-AI-main/js/index.js
+++ Indica-AI-main/js/index.js
@@ -78,7 +78,6 @@ document.addEventListener('DOMContentLoaded', () =>
 
 
 function buttonClick(){
-	console.log('click2')
 	const userInput = document.getElementById('userInput').value.trim();
 	AIFlow(userInput, true);
 }
@@ -120,7 +119,7 @@ function startRecording()
 	{
 		recognition.stop();
 		isRecording = false;
-		document.getElementById('recordButton').innerHTML = '<img src="https://raw.githubusercontent.com/Delutto/Indica-AI/main/img/mic.png">';
+		document.getElementById('recordButton').innerHTML = '<img src="./img/mic2.png">';
 		transcript = document.getElementById('userInput').value.trim();
 		AIFlow(transcript, true);		
 	};
@@ -141,3 +140,41 @@ function readMessage(text)
 
 	speechSynthesis.speak(speech);
 }
+
+
+document.getElementById('alterarContraste').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    document.getElementById('chatContainer').classList.toggle('dark-mode');
+
+	// Seleciona todos os botões de imagens dentro da div com classe chat-input-buttons
+    var buttons = document.querySelectorAll('img');
+
+    // Array com os caminhos das imagens para o modo claro e escuro
+    var lightModeImages = ['./img/send.png','./img/mic2.png', './img/hearing2.png', './img/contrast2.png', './img/zoom.png', './img/chat.png'];
+    var darkModeImages = ['./img/send.png','./img/micDark.png', './img/hearingDark.png', './img/contrastDark.png', './img/zoomDark.png', './img/chatDark.png'];
+
+    // Verifica se o body tem a classe dark-mode para decidir qual conjunto de imagens usar
+    var modeImages = document.body.classList.contains('dark-mode') ? darkModeImages : lightModeImages;
+
+    // Atualiza o src de cada imagem de acordo com o modo
+    buttons.forEach(function(button, index) {
+        button.src = modeImages[index];
+    });
+});
+
+
+document.getElementById('alterarTamanho').addEventListener('click', (e)=>{
+	const texts = document.querySelectorAll('.text');
+
+	texts.forEach((text) =>{
+		let currentFontSize = parseFloat(window.getComputedStyle(text).fontSize);
+
+		if (currentFontSize < 22) {
+            // Aumenta a fonte em 2px
+            text.style.fontSize = (currentFontSize + 2) + 'px';
+        } else {
+            // Se atingir 22px, volta para o tamanho padrão (16px)
+            text.style.fontSize = '16px';
+        }
+	})
+})
